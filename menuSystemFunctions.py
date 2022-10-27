53600e68a993fd9960c4558ec9ce56676b3bb587
Arvid Nilsson <arvid.nilsson5@learnet.se>
2022-10-27 10:57:05 +0200

    adding harry potter & alien minigames

--- menuSystemFunctions.py
+++ menuSystemFunctions.py
@@ -33,7 +33,7 @@ def laget():
             print(" ", x, end='')
         print(" ")
         
-#Pappegojan polly minigame
+#Pappegojan polly minigamegit
 def polly():
     from encodings import utf_8
     import random
@@ -68,3 +68,59 @@ def polly():
         seads = "\b\b\binte uppleva detta vädret"
 
     print(f"polly vill ha {seads} {preference} \u2665")
+
+#Harry potter house mini game
+def harryPotter():
+    import random
+    sum = 0
+    house = ""
+    randomHouse = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"]
+
+    #Check so that the sum of the traits is greater than 10
+    while sum <= 10:
+        brave = int(input("Hur modig är du på en skala av 1 till 10? "))
+        astute = int(input("Hur slug är du på en skala av 1 till 10? "))
+        patience = int(input("Hur tålmodig är du på en skala av 1 till 10? "))
+        sensible = int(input("Hr förnuftig är du på en skala av 1 till 10? "))
+        houseList = [brave, astute, patience, sensible]
+        sum = brave + astute + patience + sensible
+
+    #finds largest number in list
+    indexLargestNumber = houseList.index(max(houseList))
+
+    if brave >= 8 and astute <= 3:
+        house += "Gryffindor"
+        print(f"Ditt elevhem är {house} \nGrattis!")
+    elif astute >= 8 and brave <= 3:
+        house += "Slytherin"
+        print(f"Ditt elevhem är {house} \nGrattis!")
+    elif patience >= 8 and sensible <= 3:
+        house += "Hufflepuff"
+        print(f"Ditt elevhem är {house} \nGrattis!")
+    elif sensible >= 8 and patience <= 3:
+        house += "Ravenclaw"
+        print(f"Ditt elevhem är {house} \nGrattis!")
+    elif houseList[indexLargestNumber] <= 7 and houseList[indexLargestNumber] >= 4:
+        #find string from list by index
+        print(f"Ditt elevhem är {randomHouse[indexLargestNumber]} \nGrattis")
+    else: 
+        house += random.choice(randomHouse)
+        print(f"Ditt elevhem är {house} \nGrattis!")
+
+#Alien minigame
+def alien():
+    Alien = str(input("Vilken färg är din Alien ")).lower()
+    poäng = 0
+    if (Alien == "rosa"):
+        poäng += 15
+    elif (Alien == "blå"):
+        poäng += 10
+    elif (Alien == "gul"):
+        poäng += 5
+    print("Du har ",poäng," poäng") 
+
+
+
+
+
+        
